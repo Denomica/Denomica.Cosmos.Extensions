a7accc108a5319777306f3d00f970edb8ab6ef36
Mika Berglund <mika.berglund@iki.fi>
2021-12-29 07:37:38 +0200

    First extension method.

---
+++ Denomica.Cosmos.Extensions/CosmosExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denomica.Cosmos.Extensions
+{
+    public static class CosmosExtensions
+    {
+        public static async Task<T> FirstOrDefaultAsync<T>(this Container container, Expression<Func<T, bool>> predicate)
+        {
+            var iterator = container
+                .GetItemLinqQueryable<T>()
+                .Where(predicate)
+                .ToFeedIterator();
+
+            if(iterator.HasMoreResults)
+            {
+                var next = await iterator.ReadNextAsync();
+                return next.Resource.FirstOrDefault();
+            }
+
+            return default;
+        }
+    }
+}
