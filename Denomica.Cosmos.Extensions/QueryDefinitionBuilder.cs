81649a038c93e1ccbd34c30ea88c22c7fa96d1d1
Mika Berglund <mika.berglund@iki.fi>
2022-04-10 00:17:17 +0300

    Added documentation and a few additional methods to the QueryDefinitionBuilder class.

--- Denomica.Cosmos.Extensions/QueryDefinitionBuilder.cs
+++ Denomica.Cosmos.Extensions/QueryDefinitionBuilder.cs
@@ -6,16 +6,29 @@ using System.Threading.Tasks;
 
 namespace Denomica.Cosmos.Extensions
 {
+    /// <summary>
+    /// A builder class that you use to build <see cref="QueryDefinition"/> instances with
+    /// a fluent interface.
+    /// </summary>
     public class QueryDefinitionBuilder
     {
 
         private StringBuilder QueryTextBuilder = new StringBuilder();
+        /// <summary>
+        /// Appends a string to the query text.
+        /// </summary>
+        /// <param name="text">The string to append.</param>
         public QueryDefinitionBuilder AppendQueryText(string text)
         {
             this.QueryTextBuilder.Append(text);
             return this;
         }
 
+        /// <summary>
+        /// Appends a string to the query text if <paramref name="condition"/> is <c>true</c>.
+        /// </summary>
+        /// <param name="text">The string to conditionally append to the query text.</param>
+        /// <param name="condition">The condition that determines whether the given string should be appended to the query text.</param>
         public QueryDefinitionBuilder AppendQueryTextIf(string text, bool condition)
         {
             if (condition)
@@ -26,6 +39,11 @@ namespace Denomica.Cosmos.Extensions
             return this;
         }
 
+        /// <summary>
+        /// Appends a string to the query text if <paramref name="condition"/> returns <c>true</c>.
+        /// </summary>
+        /// <param name="text">The string to conditionally append to the query text.</param>
+        /// <param name="condition">A delegate that returns a boolean value indicateing whether the given string should be appended to the query text.</param>
         public async Task<QueryDefinitionBuilder> AppendQueryTextIfAsync(string text, Func<Task<bool>> condition)
         {
             if(await condition())
@@ -37,12 +55,51 @@ namespace Denomica.Cosmos.Extensions
         }
 
         private List<KeyValuePair<string, object>> Parameters = new List<KeyValuePair<string, object>>();
+        /// <summary>
+        /// Adds a parameter to the query definition.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="value">The value of the parameter.</param>
         public QueryDefinitionBuilder WithParameter(string name, object value)
         {
             this.Parameters.Add(new KeyValuePair<string, object>(name, value));
             return this;
         }
 
+        /// <summary>
+        /// Adds a parameter to the query if <paramref name="condition"/> is <c>true</c>.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="value">The value of the parameter.</param>
+        /// <param name="condition">The condition that determines whether the specified parameter is added to the query definition.</param>
+        public QueryDefinitionBuilder WithParameterIf(string name, object value, bool condition)
+        {
+            if(condition)
+            {
+                this.WithParameter(name, value);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Adds a parameter to the query if <paramref name="condition"/> returns <c>true</c>.
+        /// </summary>
+        /// <param name="name">The name of the parameter.</param>
+        /// <param name="value">The value of the parameter.</param>
+        /// <param name="condition">A delegate that returns a boolean value indicating whether the given parameter should be added to the query definition.</param>
+        /// <returns></returns>
+        public async Task<QueryDefinitionBuilder> WithParameterIfAsync(string name, object value, Func<Task<bool>> condition)
+        {
+            if(await condition())
+            {
+                this.WithParameter(name, value);
+            }
+            return this;
+        }
+
+        /// <summary>
+        /// Builds the query definition and returns it.
+        /// </summary>
         public QueryDefinition Build()
         {
 
@@ -55,5 +112,6 @@ namespace Denomica.Cosmos.Extensions
 
             return qry;
         }
+
     }
 }
